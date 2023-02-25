3b550a5ffd05ebb9a8aadd18588215bac8665f69
marabakumova <abakumova.ma.le@gmail.com>
2023-02-25 10:55:26 +0300

    Добавила домашнюю работу

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,9 +1,25 @@
-from selene.support.shared import browser
+import pytest
 from selene import be, have
+from selene.support.shared import browser
 
 
-browser.open('https://google.com')
-browser.config.window_height()
-browser.config.window_width()
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+@pytest.fixture
+def configured_browser():
+    browser.config.window_height = 980
+    browser.config.window_width = 670
+    return browser
+
+
+@pytest.fixture
+def define_url():
+    browser.open('https://google.com')
+
+
+def test_google_research(configured_browser, define_url):
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_google_research_negative(configured_browser, define_url):
+    browser.element('[name="q"]').should(be.blank).type('dhgfhgfgfhgdfh').press_enter()
+    browser.element('.card-section').should(have.text('По запросу dhgfhgfgfhgdfh ничего не найдено.'))
